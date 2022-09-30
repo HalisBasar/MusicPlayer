65c2f4d154c2b8e71ec28dd26a973aa493be2d17
HalisBasar <110904431+HalisBasar@users.noreply.github.com>
2022-09-30 11:04:53 +0300

    second commit

---
+++ Halis_Projeleri/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Halis_Projeleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl_1 = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.lbl_2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnOynat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_1
+            // 
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_1.Location = new System.Drawing.Point(291, 9);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(148, 26);
+            this.lbl_1.TabIndex = 0;
+            this.lbl_1.Text = "MÜZİK ÇALAR";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(63, 66);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(590, 289);
+            this.axWindowsMediaPlayer1.TabIndex = 1;
+            // 
+            // lbl_2
+            // 
+            this.lbl_2.AutoSize = true;
+            this.lbl_2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_2.Location = new System.Drawing.Point(49, 375);
+            this.lbl_2.Name = "lbl_2";
+            this.lbl_2.Size = new System.Drawing.Size(122, 22);
+            this.lbl_2.TabIndex = 2;
+            this.lbl_2.Text = "Seçilen Şarkı :";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 407);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(605, 141);
+            this.textBox1.TabIndex = 3;
+            // 
+            // btnOynat
+            // 
+            this.btnOynat.Location = new System.Drawing.Point(696, 391);
+            this.btnOynat.Name = "btnOynat";
+            this.btnOynat.Size = new System.Drawing.Size(80, 157);
+            this.btnOynat.TabIndex = 4;
+            this.btnOynat.Text = "Şarkıyı Oynat";
+            this.btnOynat.UseVisualStyleBackColor = true;
+            this.btnOynat.Click += new System.EventHandler(this.btnOynat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 568);
+            this.Controls.Add(this.btnOynat);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl_2);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.lbl_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Label lbl_2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnOynat;
+    }
+}
+
---
+++ Halis_Projeleri/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Halis_Projeleri
+{
+    public partial class Form1 : Form
+    {
+        private const string V = "Media File(*.mpg,*.dat,*.avi,*.wmv,*.wav,*.mp3)|*.wav;*.mp3;*.mpg;*.dat;*.avi;*.wmv)";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOynat_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = V;
+            //Müzik çaların çalabileceği dosyaları FileDialog sayesinde filtreledik.
+            openFileDialog1.InitialDirectory = Application.StartupPath;
+            openFileDialog1.Title = "Dosya Seç";
+            openFileDialog1.ShowDialog();
+            textBox1.Text = openFileDialog1.FileName; 
+            axWindowsMediaPlayer1.URL = textBox1.Text;
+            textBox1.Text = openFileDialog1.FileName;
+            axWindowsMediaPlayer1.URL = textBox1.Text;
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            axWindowsMediaPlayer1.Ctlcontrols.next();
+            axWindowsMediaPlayer1.Ctlcontrols.previous();
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+    }
+}
